122f4defb5edfa6f7a3efdabc426b36b095c1782
louis-25 <20151577@vision.hoseo.edu>
2021-03-18 17:18:15 +0900

    day7

--- Java_basic/day7/src/nongeneric/GenericTest.java
+++ Java_basic/day7/src/nongeneric/GenericTest.java
@@ -6,18 +6,38 @@ class Apple {
 class Paper {
 	String size = "A4";
 }
-class Box<T>{
-	T o;
-	Box(T o) {
-		this.o = o;
+
+//Box<Apple과 하위클래스, Paper>
+class Box<T, K>{// 컴파일 시점전달
+	T t1;
+	K k1;
+	String name = "상자";
+	Box(T t1, K k1) {
+		this.t1 = t1;
+		this.k1 = k1;
 	}
-	public T getO() {
-		return o;
+	public T getT1() {
+		return t1;
 	}
-	public void setO(T o) {
-		this.o = o;
+	public void setT1(T t1) {
+		this.t1 = t1;
+	}
+	public K getK1() {
+		return k1;
+	}
+	public void setK1(K k1) {
+		this.k1 = k1;
 	}
 	
+	
+}
+class BoxManager { // 클래스는 제네릭이 아니다
+	
+	//제네릭 메소드
+	public <P1, P2> Box<P1, P2> test(P1 a, P2 p) {
+		Box<P1, P2> box2 = new Box<P1, P2>(a, p);
+		return box2;
+	}
 }
 public class GenericTest {
 
@@ -26,11 +46,15 @@ public class GenericTest {
 		Apple a = new Apple();
 		Paper p = new Paper();
 		//<Apple> <Paper> 제네릭 타입사용
-		Box<Apple> appleBox = new Box<Apple>(a);
-		Box<Paper> paperBox = new Box<Paper>(p);
+		Box<Apple, Paper> box = new Box<Apple, Paper>(a, p);		
 		//제네릭 타입을 사용하면 아래와같이 코드가 간략해진다
-		System.out.println(appleBox.getO().origin);
-		System.out.println(paperBox.getO().size);
+		System.out.println(box.getT1().origin);
+		System.out.println(box.getK1().size);
+		
+		BoxManager manager = new BoxManager();
+		Box<Apple, Paper> box2 = manager.test(a, p);
+		System.out.println(box2.getT1().origin);
+		System.out.println(box2.getK1().size);
 		
 		//제네릭 타입을 사용하지 않는경우
 //		if(appleBox.getO() instanceof Apple) { //appleBox는 Apple객체인가?
