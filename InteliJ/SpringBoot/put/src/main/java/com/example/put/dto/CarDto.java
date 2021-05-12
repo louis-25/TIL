430d42cb562e1a786bbc8dce516cce201ec75c7e
louis-25 <20151577@vision.hoseo.edu>
2021-05-13 00:30:07 +0900

    PUT API 사용법

---
+++ InteliJ/SpringBoot/put/src/main/java/com/example/put/dto/CarDto.java
@@ -0,0 +1,35 @@
+package com.example.put.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class) // 스네이크로 입력받아도 인식되게해준다
+public class CarDto {
+
+    private String name;
+    private String carNumber;
+
+    @Override
+    public String toString() {
+        return "CarDto{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+}
---
+++ InteliJ/SpringBoot/put/src/main/java/com/example/put/dto/PostRequestDto.java
@@ -0,0 +1,48 @@
+package com.example.put.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+import java.util.Properties;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class) // 스네이크로 입력받아도 인식되게해준다
+public class PostRequestDto {
+
+    private String name;
+    private int age;
+    private List<CarDto> carList;
+
+    @Override
+    public String toString() {
+        return "PostRequestDto{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", carList=" + carList +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<CarDto> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(List<CarDto> carList) {
+        this.carList = carList;
+    }
+}
